293641a46c013fd525c08dd25eb01b36bade545f
kjh302903 <kjh302903@gmail.com>
2024-04-08 13:22:54 +0900

    refactor: 배경 컴포넌트 수정
    배경 컴포넌트 수정

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -7,7 +7,7 @@ interface Props {
 const Background = ({ children }: Props): JSX.Element => {
   return (
     <div className="h-screen pt-16">
-      <Image alt="background-img" src="/images/background.jpg" fill />
+      <Image alt="background-img" src="/images/background.jpg" fill className="object-cover" />
       <div className="flex items-center justify-center h-full">{children}</div>
     </div>
   );
